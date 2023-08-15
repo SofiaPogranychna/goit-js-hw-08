7e37a10e00c01adc5cdf7410c59577e1fed0d56b
SofiaPogranychna <sofiapogranychna@gmail.com>
2023-08-15 04:01:34 -0700

    fin

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+    vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
+
+vimeoPlayer.on('timeupdate', function(data) {
+    const currentTime = data.seconds;
+    updateLocalStorage(currentTime);
+});
+function updateLocalStorage(currentTime) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}
+const throttledUpdateLocalStorage = throttle(updateLocalStorage, 1000);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+const keyStorage = 'feedback-form-state'
+const savedState = localStorage.getItem(keyStorage);
+let currentState = {};
+
+if (savedState) {
+ currentState = JSON.parse(savedState);
+  form.email.value = currentState.email;
+  form.message.value = currentState.message;
+}
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() 
+{
+ currentState = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  localStorage.setItem(keyStorage, JSON.stringify(currentState));
+}
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(currentState)
+    localStorage.removeItem(keyStorage);
+    form.reset();
+
+}
+
 
